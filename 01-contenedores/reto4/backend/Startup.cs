26e30149a188371c413c71b11188b2322f3e96e4
Alejandro Miguel Florido Tejada <amft@kamstrup.com>
2025-12-31 01:13:46 +0100

    step 0 para reto4

---
+++ 01-contenedores/reto4/backend/Startup.cs
@@ -0,0 +1,59 @@
+using backend.Models;
+using backend.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace backend
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //requires using Microsoft.Extensions.Options
+            services.Configure<ClassDatabaseSettings>(Configuration.GetSection(nameof(ClassDatabaseSettings)));
+            services.AddSingleton<IClassDatabaseSettings>(sp => sp.GetRequiredService<IOptions<ClassDatabaseSettings>>().Value);
+            services.AddSingleton<ClassService>();
+
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCors("MyPolicy");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
